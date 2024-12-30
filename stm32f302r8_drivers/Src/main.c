b67cf0698055bba43967a434f9fdbcc830ea2639
Kiet Le <ktlbusiness11@gmail.com>
2024-12-30 16:45:05 -0500

    Initialize gitignore, create stm32f302r8.h header file

---
+++ stm32f302r8_drivers/Src/main.c
@@ -0,0 +1,7 @@
+#include "stm32f302r8.h"
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
---
+++ stm32f302r8_drivers/drivers/Inc/stm32f302r8.h
@@ -0,0 +1,7 @@
+
+#ifndef INC_STM32F302R8_H_
+#define INC_STM32F302R8_H_
+
+
+
+#endif /* INC_STM32F302R8_H_ */
